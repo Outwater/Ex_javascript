dda13674c6aada07674b621c2fbf5cbc3b599419
unknown <kocon135@naver.com>
2019-06-29 21:24:33 +0900

    first commit

---
+++ 5장.js
@@ -0,0 +1,67 @@
+var x;
+x=6;
+
+var str ='Hello world';
+3+2
+console.log('str')
+
+var x =5;
+var y =6;
+var z = x + y;
+
+console.log(z);
+
+
+// 함수
+function myFunction(x, y) {
+    return x + y;
+  }
+  
+  // if 문
+  if(x > 0) {
+    // do something
+  }
+  
+  // for 문
+  for (var i = 0; i < 10; i++) {
+    // do something
+  }
+
+  var time = 10;
+var greeting;
+
+if (time < 10) {
+  greeting = 'Good morning';
+} else if (time < 20) {
+  greeting = 'Good day';
+} else {
+  greeting = 'Good evening';
+}
+
+console.log(greeting);
+
+5*10>10
+
+function squre(number){
+    return number*number;
+}
+
+consonle.log(square(2))
+
+var person = {
+    name: 'Lee',
+    gender: 'male',
+    sayHello: function () {
+      console.log('Hi! My name is ' + this.name);
+    }
+  };
+  
+  console.log(typeof person); // object
+  console.log(person); // { name: 'Lee', gender: 'male', sayHello: [Function: sayHello] }
+  
+  person.sayHello(); // Hi! My name is Lee
+  person.name();
+
+  var arr=[1,2,3,4,5];
+console.log(arr[5]);
+
