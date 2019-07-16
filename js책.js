9987c305083a4a053e2f57bb272f172225a22524
unknown <kocon135@naver.com>
2019-07-16 19:17:06 +0900

    0716

---
+++ js책.js
@@ -0,0 +1,11 @@
+<script>
+var answer = confirm("송원이입니다. 인수나옵니까?")
+if(answer==true){
+    document.write("<h3>인수 등장</h3>");
+    document.write("<img src='인수나와라.jpg />"); 
+}
+else {
+    document.write("<h3>송원 등장</h3>");
+    document.write("<img src='송원이나와라.jpg />"); 
+} 
+</script>
