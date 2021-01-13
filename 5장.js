b191572920775ef6b3ae0a4eed6cbbc72bd36013
Outwater <kocon135@gmail.com>
2021-01-13 18:34:06 +0900

    test2

--- 5장.js
+++ 5장.js
@@ -1,12 +1,12 @@
 var x;
-x=6;
+x = 6;
 
-var str ='Hello world';
-3+2
+var str = 'Hello world';
+3 + 2
 console.log('str')
 
-var x =5;
-var y =6;
+var x = 50000;
+var y = 6;
 var z = x + y;
 
 console.log(z);
@@ -14,20 +14,20 @@ console.log(z);
 
 // 함수
 function myFunction(x, y) {
-    return x + y;
-  }
-  
-  // if 문
-  if(x > 0) {
-    // do something
-  }
-  
-  // for 문
-  for (var i = 0; i < 10; i++) {
-    // do something
-  }
+  return x + y;
+}
 
-  var time = 10;
+// if 문
+if (x > 0) {
+  // do something
+}
+
+// for 문
+for (var i = 0; i < 10; i++) {
+  // do something
+}
+
+var time = 10;
 var greeting;
 
 if (time < 10) {
@@ -40,29 +40,29 @@ if (time < 10) {
 
 console.log(greeting);
 
-5*10>10
+5 * 10 > 10
 
-function square(number){
-    return number*number;
+function square(number) {
+  return number * number;
 }
 
 console.log(square(2))
 
 var person = {
-    name: 'Lee',
-    gender: 'male',
-    sayHello: function () {
-      console.log('Hi! My name is ' + this.name);
-    }
-  };
-  
-  console.log(typeof person); // object
-  console.log(person); // { name: 'Lee', gender: 'male', sayHello: [Function: sayHello] }
-  
-  person.sayHello(); // Hi! My name is Lee
-  console.log(person.name);
+  name: 'Lee',
+  gender: 'male',
+  sayHello: function () {
+    console.log('Hi! My name is ' + this.name);
+  }
+};
 
-  var arr=[1,2,3,4,5];
+console.log(typeof person); // object
+console.log(person); // { name: 'Lee', gender: 'male', sayHello: [Function: sayHello] }
+
+person.sayHello(); // Hi! My name is Lee
+console.log(person.name);
+
+var arr = [1, 2, 3, 4, 5];
 console.log(arr[0]);
 
 
